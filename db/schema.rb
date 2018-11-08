a6dacb415efa5fe7d9ad42332eaac22c6b25fd86
Milos Komlenic <komlenicm96@gmail.com>
2018-11-08 18:49:22 +0100

    create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181104130312) do
+ActiveRecord::Schema.define(version: 20181105094756) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
